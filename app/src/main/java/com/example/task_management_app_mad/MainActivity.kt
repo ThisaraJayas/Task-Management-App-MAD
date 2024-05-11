f49a6fc68d9c277d18d148c2e56436172880503e
Thisara <sasmithajayasinghe1@gmail.com>
2024-05-11 22:28:37 +0530

    changes

--- app/src/main/java/com/example/task_management_app_mad/MainActivity.kt
+++ app/src/main/java/com/example/task_management_app_mad/MainActivity.kt
@@ -1,11 +1,20 @@
 package com.example.task_management_app_mad
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.task_management_app_mad.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var binding:ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        binding.addButton.setOnClickListener{
+            val intent = Intent(this, AddTaskActivity::class.java)
+            startActivity(intent)
+        }
     }
 }
---
+++ app/src/main/java/com/example/task_management_app_mad/Task.kt
@@ -0,0 +1,3 @@
+package com.example.task_management_app_mad
+
+data class Task()
