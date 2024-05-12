c070ac34b192239f60d951dedfaaf017d4c958ce
Thisara <sasmithajayasinghe1@gmail.com>
2024-05-12 17:46:39 +0530

    delete function

--- app/src/main/java/com/example/task_management_app_mad/TaskAdapter.kt
+++ app/src/main/java/com/example/task_management_app_mad/TaskAdapter.kt
@@ -7,15 +7,19 @@ import android.view.View
 import android.view.ViewGroup
 import android.widget.ImageView
 import android.widget.TextView
+import android.widget.Toast
 import androidx.recyclerview.widget.RecyclerView
 
 class TaskAdapter(private var task: List<Task>, context: Context):
     RecyclerView.Adapter<TaskAdapter.TaskViewHolder>() {
 
+        private val db:TaskDatabaseHelper = TaskDatabaseHelper(context)
     class TaskViewHolder(itemView:View):RecyclerView.ViewHolder(itemView){
         val titleTextView:TextView = itemView.findViewById(R.id.titleTextView)
         val contentTextView:TextView = itemView.findViewById(R.id.contentTextView)
         val updateButton:ImageView = itemView.findViewById(R.id.updateButton)
+        val deleteButton:ImageView = itemView.findViewById(R.id.deleteButton)
+
     }
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): TaskViewHolder {
@@ -35,6 +39,11 @@ class TaskAdapter(private var task: List<Task>, context: Context):
                 putExtra("task_id",task.id)
             }
             holder.itemView.context.startActivity(intent)
+        }
+        holder.deleteButton.setOnClickListener {
+            db.deleteTask(task.id)
+            refreshData(db.getAllTasks())
+            Toast.makeText(holder.itemView.context,"Task Deleted Success",Toast.LENGTH_SHORT).show()
         }
     }
     fun refreshData(newTask :List<Task>){
--- app/src/main/java/com/example/task_management_app_mad/TaskDatabaseHelper.kt
+++ app/src/main/java/com/example/task_management_app_mad/TaskDatabaseHelper.kt
@@ -78,4 +78,11 @@ class TaskDatabaseHelper(context:Context):SQLiteOpenHelper(context, DATABASE_NAM
         db.close()
         return Task(id, title, content)
     }
+    fun deleteTask(taskId: Int){
+        val db = writableDatabase
+        val whereClause = "$COLUMN_ID=?"
+        val whereArgs = arrayOf(taskId.toString())
+        db.delete(TABLE_NAME,whereClause,whereArgs)
+        db.close()
+    }
 }
